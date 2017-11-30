6e8a7d8606f50d93362679514c9752f0dd2cef98
Taironn <demeterd94@gmail.com>
2017-12-01 00:41:03 +0100

    Add project files.

---
+++ GradientMatrixFactorisation/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GradientMatrixFactorisation
+{
+	class Program
+	{
+		public const int movie_count = 3952 + 1;
+		public const int user_count = 6040 + 1;
+		public static double[] U, V;
+		public static Tuple<int, int, int>[] ratings;
+		public static Tuple<int, int, int>[] testingset;
+		public static Tuple<int, int>[] test;
+		public static double epsilon = 0.1;
+		static void Main(string[] args)
+		{
+
+			#region InputHandling
+
+			string[] data = System.IO.File.ReadAllLines("./moviedata/ratings.train");
+
+			//Contains all ratings in UserID, MovieID, Rating form
+			int i = 0;
+			int j = (int)(data.Length * 0.2);
+			ratings = new Tuple<int, int, int>[j];
+			testingset = new Tuple<int, int, int>[data.Length-j];
+			//foreach (var item in data)
+			//{
+			//	string[] splitted = item.Split(' ');
+			//	ratings[i++] = new Tuple<int, int, int>(int.Parse(splitted[0]), int.Parse(splitted[1]), int.Parse(splitted[2]));
+			//}
+			for (int k = 0; k < j; k++)
+			{
+				string[] splitted = data[k].Split(' ');
+				ratings[k] = new Tuple<int, int, int>(int.Parse(splitted[0]), int.Parse(splitted[1]), int.Parse(splitted[2]));
+			}
+			for (int k = j; k < data.Length; k++)
+			{
+				string[] splitted = data[k].Split(' ');
+				testingset[k-j] = new Tuple<int, int, int>(int.Parse(splitted[0]), int.Parse(splitted[1]), int.Parse(splitted[2]));
+			}
+
+			data = System.IO.File.ReadAllLines("./moviedata/ratings.test");
+
+			//Contains all test data in UserID, MovieID form
+			test = new Tuple<int, int>[data.Length];
+			i = 0;
+			foreach (var item in data)
+			{
+				string[] splitted = item.Split(' ');
+				test[i++] = new Tuple<int, int>(int.Parse(splitted[0]), int.Parse(splitted[1]));
+			}
+			#endregion
+
+			//Basic implementation here
+			//basicImplementation();
+
+			//Vectorial implementation, the dimension for the vectors in U and V is the first parameter
+			VectorialImplementation syst = new VectorialImplementation(20, ratings, test);
+			for (int k = 0; k < 500; k++)
+			{
+				syst.iterateAlgorithm(0.0025);
+				Console.WriteLine("{0} iterations done.", k + 1);
+				Console.WriteLine("Current error on learning set is: {0}", syst.sqareSetError(ratings));
+				Console.WriteLine("Current error on testing  set is: {0}", syst.sqareSetError(testingset));
+			}
+
+			//Console.WriteLine("Current error is: {0}", syst.sqareSetError());
+
+			//Show calculated values
+			//syst.showResults();
+
+			Console.ReadKey();
+		}
+
+		static void basicImplementation()
+		{
+			Random random = new Random();
+			U = Enumerable
+				.Repeat(1.0f, user_count)
+				.Select(x => random.NextDouble() * Math.Sqrt(5))
+				.ToArray();
+
+			V = Enumerable
+				.Repeat(1.0f, movie_count)
+				.Select(x => random.NextDouble() * Math.Sqrt(5))
+				.ToArray();
+
+			//Iterate the algorithm
+			for (int j = 0; j < 5; j++)
+			{
+				GradientIteration();
+				Console.WriteLine("{0} iterations done.", j + 1);
+			}
+
+			//Write predicition of test data
+			foreach (var item in test)
+			{
+				Console.WriteLine("{0} {1} -> {2}", item.Item1, item.Item2, U[item.Item1] * V[item.Item2]);
+				Thread.Sleep(500);
+			}
+
+			Console.ReadKey();
+		}
+
+		static void GradientIteration()
+		{
+			foreach (var item in ratings)
+			{
+				//The current estimation of the model
+				double cur_value = U[item.Item1] * V[item.Item2];
+
+				//The current error of the model
+				double cur_diff = item.Item3 - cur_value;
+
+				//Update model according to gradient
+				U[item.Item1] += epsilon * cur_diff;
+				V[item.Item2] += epsilon * cur_diff;
+			}
+		}
+	}
+}
---
+++ GradientMatrixFactorisation/VectorialImplementation.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GradientMatrixFactorisation
+{
+	class VectorialImplementation
+	{
+		public const int movie_count = 3952 + 1;
+		public const int user_count = 6040 + 1;
+		public static double epsilon = 0.005;
+
+		public double[][] U, V;
+		private Tuple<int, int, int>[] ratings;
+		//private Tuple<int, int, int>[] testset;
+		private Tuple<int, int>[] test;
+		private int dimension;
+		public VectorialImplementation(int dimension, Tuple<int, int, int>[] ratings, Tuple<int, int>[] test)
+		{
+			this.ratings = ratings;
+			this.test = test;
+			this.dimension = dimension;
+			//this.testset = testset;
+			Random random = new Random();
+
+			//Initialize system with given dimension parameter
+			U = new double[user_count][];
+			for (int i = 0; i < U.Length; i++)
+			{
+				U[i] = Enumerable
+					.Repeat(1.0d, dimension)
+					.Select(x => random.NextDouble() )//* Math.Sqrt(5))
+					.ToArray();
+			}
+
+			V = new double[movie_count][];
+			for (int i = 0; i < V.Length; i++)
+			{
+				V[i] = Enumerable
+					.Repeat(1.0d, dimension)
+					.Select(x => random.NextDouble() )//* Math.Sqrt(5))
+					.ToArray();
+			}
+
+		}
+
+		public void iterateAlgorithm(double lambda = -1.0)
+		{
+			if (lambda <= 0)
+				foreach (var item in ratings)
+				{
+					//Copying values, since array is a reference type
+					double[] cur_u = new double[dimension];
+					double[] cur_v = new double[dimension];
+					for (int i = 0; i < dimension; i++)
+					{
+						cur_u[i] = U[item.Item1][i];
+						cur_v[i] = V[item.Item2][i];
+					}
+
+					//The current estimation of the model
+					double cur_value = dot(cur_u, cur_v);
+
+					//The current error of the model
+					double cur_diff = item.Item3 - cur_value;
+
+					//Modify model
+					for (int i = 0; i < dimension; i++)
+					{
+						//Weight modification with the value in dot product for better optimization
+						U[item.Item1][i] += cur_diff * epsilon * cur_v[i];
+						V[item.Item2][i] += cur_diff * epsilon * cur_u[i];
+						//if (U[item.Item1][i] >= 100)
+						//{
+						//	Console.WriteLine("NaN");
+						//}
+					}
+
+				}
+			else
+				foreach (var item in ratings)
+				{
+					//Copying values, since array is a reference type
+					double[] cur_u = new double[dimension];
+					double[] cur_v = new double[dimension];
+					for (int i = 0; i < dimension; i++)
+					{
+						cur_u[i] = U[item.Item1][i];
+						cur_v[i] = V[item.Item2][i];
+					}
+
+					//The current estimation of the model
+					double cur_value = dot(cur_u, cur_v);
+
+					//The current error of the model
+					double cur_diff = item.Item3 - cur_value;
+
+					//Modify model
+					for (int i = 0; i < dimension; i++)
+					{
+						//Weight modification with the value in dot product for better optimization
+						//Prevent overlearning by subtracting lambda at correction
+						U[item.Item1][i] += cur_diff * epsilon * cur_v[i] - lambda * cur_u[i];
+						V[item.Item2][i] += cur_diff * epsilon * cur_u[i] - lambda * cur_v[i];
+					}
+				}
+		}
+
+		public double dot(double[] curU, double[] curV)
+		{
+			double result = 0;
+			for (int i = 0; i < dimension; i++)
+			{
+				result += curU[i] * curV[i];
+			}
+			return result;
+		}
+
+		public double squareSum(double[] value)
+		{
+			double result = 0;
+			foreach (var item in value)
+			{
+				result += item * item;
+			}
+			return result;
+		}
+
+		public void showResults()
+		{
+			foreach (var item in test)
+			{
+				Console.WriteLine("{0} {1} -> {2}", item.Item1, item.Item2, dot(U[item.Item1],V[item.Item2]));
+				Thread.Sleep(500);
+			}
+		}
+
+		public double sqareSetError(Tuple<int, int, int>[] ratings)
+		{
+			double error = 0;
+			foreach (var item in ratings)
+			{
+
+				//The current estimation of the model
+				double cur_value = dot(U[item.Item1], V[item.Item2]);
+
+				//The current error of the model
+				double cur_diff = item.Item3 - cur_value;
+
+				//Square error
+				error += (cur_diff * cur_diff) / ratings.Length;
+			}
+
+			return error;
+		}
+	}
+}
